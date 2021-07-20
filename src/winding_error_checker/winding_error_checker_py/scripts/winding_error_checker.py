b1ec87aae8b7acab0d85c76165f0fd3f1c2af854
Sergei <sejego@taltech.ee>
2021-07-20 13:06:29 +0300

    fix python copy of Winding Checker

--- src/winding_error_checker/winding_error_checker_py/scripts/winding_error_checker.py
+++ src/winding_error_checker/winding_error_checker_py/scripts/winding_error_checker.py
@@ -6,7 +6,7 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from digital_twin_msgs.msg import Current
 import threading
-import math
+from math import pow, sqrt
 
 SIZE_A = 5000
 
@@ -22,13 +22,15 @@ class WindingErrorChecker(Node):
             10)
 
         self.currents_listener_  # prevent unused variable warning
-        self.warnings_publisher_ = self.create_publisher(String, 'diagnostics/warnings/windings', 10)
+        self.warnings_publisher_ = self.create_publisher(String, 'diagnostics/windings/warnings', 10)
         
         self.i = 0
         self.currents_= [[] for i in range(3)]
         self.currents_buf = [[] for i in range(3)]
         self.can_calculate = False
         phase_check_thread = threading.Thread(target = self.phase_checker, daemon = True)
+        phase_check_thread.start()
+        self.get_logger().info('Init happened')
 
     def publish_warning(self):
         msg = String()
@@ -36,17 +38,22 @@ class WindingErrorChecker(Node):
         self.warnings_publisher_.publish(msg)
 
     def current_callback(self, msg):
-        if(self.i < SIZE_A):
+        if(self.i >= SIZE_A):
             self.i = 0
             self.currents_buf_ = self.currents_
             self.can_calculate = True
             self.currents_ = [[] for i in range(3)]
 
-
-        self.currents_.insert(1, msg.current1)
-        self.currents_.insert(2, msg.current2)
-        self.currents_.insert(3, msg.current3)
+        self.currents_[0].append(msg.current1)
+        self.currents_[1].append(msg.current2)
+        self.currents_[2].append(msg.current3)
         self.i += 1
+
+    #def check_with_matlab(self):
+    #    eng = matlab.engine.start_matlab()
+    #    ret = eng.triarea(1.0,5.0)
+    #    eng.quit()
+    #    return ret
 
     def phase_checker(self):
         square = [0, 0, 0]
@@ -56,26 +63,25 @@ class WindingErrorChecker(Node):
 
         while True:
             if self.can_calculate:
-                print('can calculate')
                 for sq in range(SIZE_A):
-                    square[0] += pow(self.currents_buf_[0][sq], self.currents_buf_[0][sq])
-                    square[1] += pow(self.currents_buf_[1][sq], self.currents_buf_[1][sq])
-                    square[2] += pow(self.currents_buf_[2][sq], self.currents_buf_[2][sq])
+                    square[0] += pow(self.currents_buf_[0][sq], 2)
+                    square[1] += pow(self.currents_buf_[1][sq], 2)
+                    square[2] += pow(self.currents_buf_[2][sq], 2)
 
                 for m in range(3):
                     mean[m] = square[m] / SIZE_A
 
                 for r in range(3):
-                    root[r] = math.sqrt(mean[r])
+                    root[r] = sqrt(mean[r])
 
                 for e in range(3):
                     error_coeff[e] = root[e] / root[0]
 
                 for err in error_coeff:
                     if abs(error_coeff[0] - err) >= 0.15:
+                        self.get_logger().warn('Potential winding problem')
                         self.publish_warning()
-
-            self.can_calculate = False
+                self.can_calculate = False
 
 def main(args=None):
     rclpy.init(args=args)
