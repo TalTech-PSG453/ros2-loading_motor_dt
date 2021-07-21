09d4f22639134e26c45545fc9fc55737ece37f2d
Sergei <sejego@taltech.ee>
2021-07-21 11:23:02 +0300

    fix identation and nullification of squares. Simplify code

--- src/winding_error_checker/winding_error_checker_py/scripts/winding_error_checker.py
+++ src/winding_error_checker/winding_error_checker_py/scripts/winding_error_checker.py
@@ -7,6 +7,8 @@ from std_msgs.msg import String
 from digital_twin_msgs.msg import Current
 import threading
 from math import pow, sqrt
+# import matlab.engine
+# import sys
 
 SIZE_A = 5000
 
@@ -23,18 +25,25 @@ class WindingErrorChecker(Node):
 
         self.currents_listener_  # prevent unused variable warning
         self.warnings_publisher_ = self.create_publisher(String, 'diagnostics/windings/warnings', 10)
+        #self.analysis_publisher_ = self.create_publisher(String, 'diagnostics/windings/park_clarke', 10)
         
         self.i = 0
         self.currents_= [[] for i in range(3)]
         self.currents_buf = [[] for i in range(3)]
         self.can_calculate = False
         phase_check_thread = threading.Thread(target = self.phase_checker, daemon = True)
+        #matlab_thread = threading.Thread(target = self.matlab_analysis)
         phase_check_thread.start()
         self.get_logger().info('Init happened')
 
     def publish_warning(self):
         msg = String()
         msg.data = 'Winding warning'
+        self.warnings_publisher_.publish(msg)
+    
+    def publish_clarke_park(self):
+        msg = String()
+        msg.data = 'TEST MESSAGE'
         self.warnings_publisher_.publish(msg)
 
     def current_callback(self, msg):
@@ -49,38 +58,44 @@ class WindingErrorChecker(Node):
         self.currents_[2].append(msg.current3)
         self.i += 1
 
-    #def check_with_matlab(self):
+    #def matlab_analysis(self, buffer):
     #    eng = matlab.engine.start_matlab()
-    #    ret = eng.triarea(1.0,5.0)
-    #    eng.quit()
-    #    return ret
+    #    x, y = eng.ParkClarke(buffer[0], buffer[1], buffer[2])
+    #    print(type(x))
+    #    print(type(y))
+    #    print('-----')
+    #    print(sys.getsizeof(x))
+    #    print(sys.getsizeof(y))
 
-    def phase_checker(self):
+    def get_rms(self, buffer):
         square = [0, 0, 0]
         mean = [0, 0, 0]
         root = [0, 0, 0]
-        error_coeff = [0, 0, 0]
 
+        for sq in range(SIZE_A):
+            square[0] += pow(buffer[0][sq], 2)
+            square[1] += pow(buffer[1][sq], 2)
+            square[2] += pow(buffer[2][sq], 2)
+
+        for m in range(3):
+            mean[m] = square[m] / SIZE_A
+        for r in range(3):
+            root[r] = sqrt(mean[r])
+        return root
+
+    def phase_checker(self):
+        error_coeff = [0, 0, 0]
         while True:
             if self.can_calculate:
-                for sq in range(SIZE_A):
-                    square[0] += pow(self.currents_buf_[0][sq], 2)
-                    square[1] += pow(self.currents_buf_[1][sq], 2)
-                    square[2] += pow(self.currents_buf_[2][sq], 2)
-
-                for m in range(3):
-                    mean[m] = square[m] / SIZE_A
-
-                for r in range(3):
-                    root[r] = sqrt(mean[r])
-
+                rms = self.get_rms(self.currents_buf_)
                 for e in range(3):
-                    error_coeff[e] = root[e] / root[0]
+                    error_coeff[e] = rms[e] / rms[0]
 
                 for err in error_coeff:
                     if abs(error_coeff[0] - err) >= 0.15:
                         self.get_logger().warn('Potential winding problem')
                         self.publish_warning()
+
                 self.can_calculate = False
 
 def main(args=None):
