13c3bffd252978f004eb55421ad59b0e57ef8834
sejego <sejego@taltech.ee>
2022-01-27 00:25:48 +0200

    remove race conditions in torque_calculator

--- src/torque_calculator/TorqueCalculator.cpp
+++ src/torque_calculator/TorqueCalculator.cpp
@@ -5,6 +5,8 @@
 #include <chrono>
 #include <memory>
 #include <functional>
+#include <mutex>
+
 #include "rclcpp/rclcpp.hpp"
 #include <digital_twin_msgs/msg/power.hpp>
 #include <digital_twin_msgs/msg/float32_stamped.hpp>
@@ -18,112 +20,96 @@ using namespace message_filters;
 
 class TorqueCalculator : public rclcpp::Node
 {
-private:
+    private:
 
-    bool is_velocity_updated_ = false;
-    bool is_power_updated_ = false;
-    bool is_efficiency_updated = false;
+        float electrical_power_ = 0;
+        float efficiency_ = 0;
+        float angular_velocity_= 0;
+        float mechanical_torque_ = 0;
+        float electrical_torque_ref_ = 0;
+        
+        std::mutex mutex_;
 
-    float electrical_power_ = 0;
-    float efficiency_ = 0;
-    float angular_velocity_= 0;
-    float mechanical_torque_ = 0;
-    float electrical_torque_ref_ = 0;
-
-    float calculateElectricalTorqueRef()
-    {
-        return (electrical_power_ / angular_velocity_);
-    }
-
-    float calculateMechanicalTorque()
-    {
-        return (electrical_torque_ref_ * efficiency_);
-    }
-
-    /* Shared Pointers with ROS methods */
-    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr efficiencyReceiver;
-    Subscriber<digital_twin_msgs::msg::Float32Stamped> angularVelocityReceiver;
-    Subscriber<digital_twin_msgs::msg::Power> powerReceiver;
-    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr electricalTorquePublisher;
-    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr mechanicalTorquePublisher;
-
-    // ApproximateTime Policy synchronizer definitions. This is required to approximately synchronize the incoming data from 2  different
-    // topics for further calculations. In theory, should increase accuracy. More info below:
-    // http://wiki.ros.org/message_filters
-
-    typedef sync_policies::ApproximateTime<digital_twin_msgs::msg::Power, digital_twin_msgs::msg::Float32Stamped> ApproxSyncPolicy;
-    std::shared_ptr<Synchronizer<ApproxSyncPolicy>> msg_sync_;
-    rclcpp::TimerBase::SharedPtr timer_;
-
-    std_msgs::msg::Float32 electrical_torque_msg_;
-    std_msgs::msg::Float32 mechanical_torque_msg_;
-
-
-public:
-    TorqueCalculator() : Node("torque_calculator")
-    {
-        /* Subscribers */
-
-        efficiencyReceiver = this->create_subscription<std_msgs::msg::Float32>("efficiency", 10,
-                            std::bind(&TorqueCalculator::efficiencyListener, this, std::placeholders::_1));
-
-        powerReceiver.subscribe(this, "motor_power/electrical_power");
-        angularVelocityReceiver.subscribe(this, "actual_rpm"); // shaft_angular_velocity
-
-        // The way synchronizer is implemented can be found below:
-        // https://answers.ros.org/question/366440/ros-2-message_filters-timesynchronizer-minimal-example-does-not-reach-callback-function/
-
-        msg_sync_ = std::make_shared<Synchronizer<ApproxSyncPolicy>>(ApproxSyncPolicy(100),powerReceiver, angularVelocityReceiver);
-        msg_sync_->registerCallback(std::bind(&TorqueCalculator::syncCallback, this, std::placeholders::_1, std::placeholders::_2));
-
-        electrical_torque_msg_.data = 0;
-        mechanical_torque_msg_.data = 0;
-
-        /* Publishers */
-        electricalTorquePublisher = this->create_publisher<std_msgs::msg::Float32>("electrical_torque_ref", 10);
-        mechanicalTorquePublisher = this->create_publisher<std_msgs::msg::Float32>("mechanical_torque", 10);
-
-        timer_ = this->create_wall_timer(100ms, std::bind(&TorqueCalculator::publishTorques, this)); // 100ms = 10 Hz
-        }
-
-        float getMechanicalTorque()
+        float calculateElectricalTorqueRef()
         {
-            return mechanical_torque_;
+            return (electrical_power_ / angular_velocity_);
         }
 
-        float getElectricalTorqueRef()
+        float calculateMechanicalTorque()
         {
-            return electrical_torque_ref_;
+            return (electrical_torque_ref_ * efficiency_);
         }
 
-        void publishTorques()
+        /* Shared Pointers with ROS methods */
+        Subscriber<digital_twin_msgs::msg::Float32Stamped> angularVelocityReceiver;
+        Subscriber<digital_twin_msgs::msg::Power> powerReceiver;
+        Subscriber<digital_twin_msgs::msg::Float32Stamped> efficiencyReceiver;
+        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr electricalTorquePublisher;
+        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr mechanicalTorquePublisher;
+
+        // ApproximateTime Policy synchronizer definitions. This is required to approximately synchronize the incoming data from 2  different
+        // topics for further calculations. In theory, should increase accuracy. More info below:
+        // http://wiki.ros.org/message_filters
+
+        typedef sync_policies::ApproximateTime<digital_twin_msgs::msg::Power, digital_twin_msgs::msg::Float32Stamped, digital_twin_msgs::msg::Float32Stamped> ApproxSyncPolicy;
+        std::shared_ptr<Synchronizer<ApproxSyncPolicy>> msg_sync_;
+        rclcpp::TimerBase::SharedPtr timer_;
+
+        std_msgs::msg::Float32 electrical_torque_msg_;
+        std_msgs::msg::Float32 mechanical_torque_msg_;
+
+
+    public:
+        TorqueCalculator() : Node("torque_calculator")
         {
-            electrical_torque_msg_.data = getElectricalTorqueRef();
-            mechanical_torque_msg_.data = getMechanicalTorque();
-            electricalTorquePublisher->publish(electrical_torque_msg_);
-            mechanicalTorquePublisher->publish(mechanical_torque_msg_);
+            /* Subscribers */
+
+            powerReceiver.subscribe(this, "motor_power/electrical_power");
+            angularVelocityReceiver.subscribe(this, "actual_rpm"); // shaft_angular_velocity
+            efficiencyReceiver.subscribe(this, "efficiency");
+
+            // The way synchronizer is implemented can be found below:
+            // https://answers.ros.org/question/366440/ros-2-message_filters-timesynchronizer-minimal-example-does-not-reach-callback-function/
+
+            msg_sync_ = std::make_shared<Synchronizer<ApproxSyncPolicy>>(ApproxSyncPolicy(100),powerReceiver, angularVelocityReceiver, efficiencyReceiver);
+            msg_sync_->registerCallback(std::bind(&TorqueCalculator::syncCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+
+            electrical_torque_msg_.data = 0.0;
+            mechanical_torque_msg_.data = 0.0;
+
+            /* Publishers */
+
+            electricalTorquePublisher = this->create_publisher<std_msgs::msg::Float32>("electrical_torque_ref", 10);
+            mechanicalTorquePublisher = this->create_publisher<std_msgs::msg::Float32>("mechanical_torque", 10);
+
+            timer_ = this->create_wall_timer(100ms, std::bind(&TorqueCalculator::publishTorques, this)); // 100ms = 10 Hz
         }
 
-        void efficiencyListener(const std_msgs::msg::Float32::SharedPtr msg)
-        {
-            efficiency_ = msg->data;
-            mechanical_torque_ = calculateMechanicalTorque();
-        }
+            // In order to avoid racing conditions in callback and publisher, mutexes are used to ensure only
+            // one method is reading/writing to variables at a time
 
-        void syncCallback(
-            const digital_twin_msgs::msg::Power::ConstSharedPtr& msg1,
-            const digital_twin_msgs::msg::Float32Stamped::ConstSharedPtr& msg2)
-        {
-            /*
-            RCLCPP_INFO(this->get_logger(),
-                "I heard and synchronized the following timestamps: %u, %u",
-                msg1->header.stamp.sec, msg2->header.stamp.sec);
-            */
-           electrical_power_ = msg1->total;
-           angular_velocity_ = msg2->data;
-           electrical_torque_ref_ = calculateElectricalTorqueRef();
-        }
-    };
+            void publishTorques()
+            {
+                const std::lock_guard<std::mutex> lock(mutex_);
+                electrical_torque_msg_.data = electrical_torque_ref_;
+                mechanical_torque_msg_.data = mechanical_torque_;
+                electricalTorquePublisher->publish(electrical_torque_msg_);
+                mechanicalTorquePublisher->publish(mechanical_torque_msg_);
+            }
+
+            void syncCallback(
+                const digital_twin_msgs::msg::Power::ConstSharedPtr& msg1,
+                const digital_twin_msgs::msg::Float32Stamped::ConstSharedPtr& msg2,
+                const digital_twin_msgs::msg::Float32Stamped::ConstSharedPtr& msg3)
+            {
+                electrical_power_ = msg1->total;
+                angular_velocity_ = msg2->data;
+                efficiency_ = msg3->data;
+                const std::lock_guard<std::mutex> lock(mutex_);
+                electrical_torque_ref_ = calculateElectricalTorqueRef();
+                mechanical_torque_ = calculateMechanicalTorque();
+            }
+};
 
 int main(int argc, char *argv[])
 {
