83dfa90786a3879e30ad22351b3d166398375e54
sejego <sejego@taltech.ee>
2022-06-28 13:52:08 +0300

    attempting to create parametrizable launch with configs

--- launch/current_simulator_launch.py
+++ launch/current_simulator_launch.py
@@ -5,6 +5,29 @@ from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import PushRosNamespace
 from launch.actions import GroupAction
+from rewritten_yaml import RewrittenYaml
+
+import yaml
+from pathlib import Path
+home = str(Path.home())
+
+def prepend_to_filenames(config_file, package_name):
+
+    with open(config_file, "r") as f:
+        config_yaml = yaml.safe_load(f)
+
+    for ns in config_yaml:
+        for node in config_yaml[ns]:
+            try:
+                filename = config_yaml[ns][node]["ros__parameters"]["filename"]
+                config_yaml[ns][node]["ros__parameters"]["filename"] = os.path.join(
+                    get_package_share_directory(package_name),'data_files', filename)
+            except:
+                pass
+    with open(os.path.join(home,"debug.yaml"),'w') as f:
+        yaml.dump(config_yaml, f)
+
+    return config_yaml
 
 def generate_launch_description():
 
@@ -16,12 +39,19 @@ def generate_launch_description():
         'config',
         'params.yaml'
     )
+    params_prepended = prepend_to_filenames(config, "loading_motor_dt")
+    
+    configured_params = RewrittenYaml(
+        source_file=config,
+        root_key='tb_tm',
+        param_rewrites=params_prepended
+        )    
 
     current_simulator=Node(
         package = 'loading_motor_dt',
         name = 'current_simulator',
         executable = 'current_simulator',
-        parameters = [config]
+        parameters = [configured_params]
     )
 
     current_simulator_with_ns = GroupAction(
--- launch/efficiency_map_launch.py
+++ launch/efficiency_map_launch.py
@@ -2,6 +2,29 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import yaml
+from pathlib import Path
+
+home = str(Path.home())
+
+def prepend_to_filenames(config_file, package_name):
+
+    with open(config_file, "r") as f:
+        config_yaml = yaml.safe_load(f)
+
+    for ns in config_yaml:
+        for node in config_yaml[ns]:
+            try:
+                filename = config_yaml[ns][node]["ros__parameters"]["filename"]
+                config_yaml[ns][node]["ros__parameters"]["filename"] = os.path.join(
+                    get_package_share_directory(package_name),'data_files', filename)
+            except:
+                pass
+    with open(os.path.join(home,"debug.yaml"),'w') as f:
+        yaml.dump(config_yaml, f)
+
+    return config_yaml
+
 def generate_launch_description():
 
     ld = LaunchDescription()
@@ -10,13 +33,15 @@ def generate_launch_description():
         'config',
         'params.yaml'
         )
-        
+    
+    params_prepended = prepend_to_filenames(config, "loading_motor_dt")
+
     data_processor=Node(
         package = 'loading_motor_dt',
-        namespace = 'tb_lm',
+        namespace = 'tb_lm_left',
         name = 'efficiency_map',
         executable = 'efficiency_map',
-        parameters = [config]
+        parameters = [params_prepended]
     )
     ld.add_action(data_processor)
     return ld
